e992d0f7f7f0d29374858e6a0c98ec45e4ef57b6
kenp1171-ui <kenpabalan1171@gmail.com>
2025-08-14 18:20:22 +0800

    fixed js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 // view gallery
-const lb = document.createElement('div');
-lb.className = 'lightbox';
-lb.innerHTML = '<img alt="Expanded" />';
-lb.addEventListener('click', ()=> lb.classList.remove('open'));
-document.body.appendChild(lb);
-const lbImg = lb.querySelector('img');
 
-document.querySelectorAll('.gallery a').forEach(a=>{
-a.addEventListener('click', e=>{
-e.preventDefault();
-lbImg.src = a.getAttribute('href');
-lb.classList.add('open');
-})
+const lb = document.createElement('div');
+    lb.className = 'lightbox';
+    lb.innerHTML = '<img alt="Expanded" />';
+    lb.addEventListener('click', ()=> lb.classList.remove('open'));
+    document.body.appendChild(lb);
+    const lbImg = lb.querySelector('img');
+
+    document.querySelectorAll('.gallery a').forEach(a=>{
+    a.addEventListener('click', e=>{
+    e.preventDefault();
+    lbImg.src = a.getAttribute('href');
+    lb.classList.add('open');
+    })
 })
