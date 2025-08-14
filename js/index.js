6710163ae1fbc8094f01ff143bbd2063d47701d3
kenp1171-ui <kenpabalan1171@gmail.com>
2025-08-14 19:36:33 +0800

    update

--- js/index.js
+++ js/index.js
@@ -14,3 +14,44 @@ const lb = document.createElement('div');
     lb.classList.add('open');
     })
 })
+
+// hide n show nav
+
+// const navbar = document.getElementById("navbar");
+// let hideTimeout;
+
+// function hideNavbar() {
+//     navbar.classList.add("hidden");
+// }
+
+// function showNavbar() {
+//     navbar.classList.remove("hidden");
+//     restartHideTimer();
+// }
+
+// function restartHideTimer() {
+//     clearTimeout(hideTimeout);
+//     hideTimeout = setTimeout(hideNavbar, 1000);
+// }
+
+// function checkAboutSection() {
+//     const aboutTop = document.queryBoundingClientRec().top;
+
+//     if (aboutTop <= 0) {
+//         restartHideTimer();
+//     } else {
+//         clearTimeout(hideTimeout);
+//     }
+// }
+
+// document.addEventListener("scroll", () => {
+//     if (window.scrollY > 100) {
+//         showNavbar();
+//     }
+// });
+// window.addEventListener("scroll", checkAboutSection);
+// checkAboutSection();
+
+
+// toggle menu
+
